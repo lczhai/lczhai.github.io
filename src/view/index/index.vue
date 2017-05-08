6273096e6220eae8217e07a4aeb95538c480b52f
翟留闯 <zhailiuchuang@simao.local>
2017-05-08 10:51:43 +0800

    init

---
+++ src/view/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  翟留闯：zhailuo@163.com
+</div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome To My GitHub'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
